ac7da6cbe288b5b48735cf77b9925adc0d61a6c8
Wallyvay <wallyvay@gmail.com>
2026-04-17 10:56:09 +0800

    Initial Commit

---
+++ Pic2Link/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pic2Link
+//
+//  Created by 天野家 on 2026/4/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pic2Link/Pic2LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pic2LinkApp.swift
+//  Pic2Link
+//
+//  Created by 天野家 on 2026/4/17.
+//
+
+import SwiftUI
+
+@main
+struct Pic2LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
